c034569897720daba2f8cfeb53d1f5de43155cd0
rohitmogal7-crypto <rohitmogal7@gmail.com>
2026-08-21 09:41:40 +0530

    Add files via upload

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,38 +1,18 @@
+//Write a program to find the factorialof any number entered by the user.
 import java.util.*;
-public class JavaBasics{
-    /**
-    
-     */
+
+public class JavaBasics {
+
     public static void main(String args[]){
-        //System.out.println("Write a program that reads a set of integers, and then prints the sum of the even and odd integers.");
-        Scanner sc= new Scanner (System.in);
-        
-        int sumeven=0;
-        int sumodd=0;
-        int choice;
-        
-            do{
-                System.out.println("Enter your number n:");
-                int n=sc.nextInt();
-                
-                if(n%2==0){
-                    sumeven=sumeven + n;
-
-                }
-                
-                else{
-                    sumodd=sumodd +n;
-                }
-
-                System.out.println("Press 1 to continue and Press 0 to exit");
-                choice = sc.nextInt();
-
-                
-            }while(choice==1);
-
-            System.out.println("sum of the even nos is:"+sumeven);
-             System.out.println("sum of the odd nos is:"+sumodd);
+        Scanner sc = new Scanner(System.in);
+        int fact = 1;
+        System.out.print("Enter num for factorial:");
+        int num = sc.nextInt();
+        for(int i=1; i<=num;i++){
+            fact*=i;
         }
-         
-
-    }
+        System.out.print("factorial for "+ num + " is " + fact);
+        
+    
+    }   
+}
